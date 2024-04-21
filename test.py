9581cf86d1161a7eb6ec189ff9a85a050d23c1d2
adityadhardwivedi <131398670+adityadhardwivedi@users.noreply.github.com>
2024-04-21 11:43:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -17,9 +17,9 @@ from streamlit_option_menu import option_menu
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Loading the saved models
-diabetes_model = pickle.load(open('E:/multiple-disease-prediction-streamlit-app-main/trainedmodel2.sav', 'rb'))
-heartdisease_model = pickle.load(open('E:/multiple-disease-prediction-streamlit-app-main/trainedmodel1.sav', 'rb'))
+ diabetes_model = pickle.load(open(f'{working_dir}/trainedmodel2.sav', 'rb'))
 
+heart_disease_model = pickle.load(open(f'{working_dir}/trainedmodel1.sav', 'rb'))
 # Sidebar for navigation
 with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
