e57155cb7eca9d934afda26351f781c32f664852
adityadhardwivedi <131398670+adityadhardwivedi@users.noreply.github.com>
2024-04-21 11:18:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ import pickle
 import streamlit as st
 import pandas as pd
 import base64
+pip install streamlit-option-menu
+
 from streamlit_option_menu import option_menu
 
 # Loading the saved models
